40642af60c6a02ecd3ce961cd3ec87fcd7ff2099
yinyan <yin.yan01@iwhalecloud.com>
2021-09-30 23:35:12 +0800

    toggle sql

--- db/index.js
+++ db/index.js
@@ -1,20 +1,20 @@
 const mysql = require('mysql')
 
 // 测试
-const db = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '123456',
-    database: 'student'
-})
+// const db = mysql.createPool({
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: '123456',
+//     database: 'student'
+// })
 
 // 正式
-// const db = mysql.createPool({
-//     host: '121.40.154.75',
-//     user: 'root',
-//     password: 'Zwlyy123456~',
-//     database: 'mysql'
-// })
+const db = mysql.createPool({
+    host: '121.40.154.75',
+    user: 'root',
+    password: 'Zwlyy123456~',
+    database: 'mysql'
+})
 
 db.query('select 1', (err, results) => {
     if (err) return console.log(err.message)
